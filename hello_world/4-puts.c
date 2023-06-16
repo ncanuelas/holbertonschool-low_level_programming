79de7bb44db2d5a554c9cff1ffc0ec3ad198f470
ncanuelas <nixacanuelas@yahoo.com>
2023-06-16 14:44:01 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,17 +1,19 @@
-
 /*
  * File: 4-puts.c
- * Auth:N C <trabis.me@icloud.com
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle *
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 
+	return (0);
+}
