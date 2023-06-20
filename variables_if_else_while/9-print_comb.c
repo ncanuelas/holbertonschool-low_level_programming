41d83bf78db75aef41f53b64c0b30b92124f4589
ncanuelas <nixacanuelas@yahoo.com>
2023-06-20 18:27:18 +0000

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -15,10 +15,11 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
+
 		if (num != 9)
-        {
-		putchar(',');
-		putchar(' ');
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
