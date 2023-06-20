6bc46b0c87a9a8147a56bc947c1b7ecbb3bbd1d8
ncanuelas <nixacanuelas@yahoo.com>
2023-06-20 17:53:58 +0000

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-#include <time.h>
-
-/**
- *main - prints all possible combinations of single-digit numbers
- *Return: 0
+/*
+ * File: 9-print_comb.c
+ * Auth: Nixaliz CT
  */
 
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int num;
