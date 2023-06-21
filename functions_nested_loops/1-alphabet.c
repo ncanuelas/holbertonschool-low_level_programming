2f70b5d31b1f1682813fdb553bd959b4e731f28b
ncanuelas <nixacanuelas@yahoo.com>
2023-06-21 20:01:54 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,22 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Nixaliz CT
+ */
+
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
+	int count = 0;
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	while (count++ <= 9)
 	{
-		_putchar(letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
