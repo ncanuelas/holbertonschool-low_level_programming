81095d9ed975de875df05d6b4e21d447afab8eb2
ncanuelas <nixacanuelas@yahoo.com>
2023-06-24 00:42:24 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+	for (len = 0; len < n; len++)
+	putchar('_');
+	}
+
+	putchar('\n');
+}
