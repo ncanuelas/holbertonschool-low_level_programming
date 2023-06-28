793028c1231ca01ea28ab6b7402439ae9a1ce2ea
ncanuelas <nixacanuelas@yahoo.com>
2023-06-28 00:18:05 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
@@ -9,6 +10,6 @@ void _puts(char *str)
 	for (; *str != '0'; str++)
 	{
 	_putchar(*str);
-	}
+
 	_putchar('\n');
 }
