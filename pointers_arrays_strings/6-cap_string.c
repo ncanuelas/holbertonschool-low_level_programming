eabb8fd27d697f31f38e7505dcbe00cd6f7e7654
ncanuelas <nixacanuelas@yahoo.com>
2023-06-29 17:42:13 +0000

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,33 +4,32 @@
  *
  */
 {
-    int i, c;
-    int trigger;
-    char nots[] = ",;.!?(){}\n\t" ";
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t" ";
 
-    for (i = 0, trigger = 0; str[i] != '\0'; i++)
-    {
-        if (str[0] > 96 && str[0] < 123)
-            trigger = 1;
-        for (c = 0; nots[c] != '\0'; c++)
-        {
-            if (nots[c] == str[i])
-                trigger = 1;
-        }
-
-        if (trigger)
-        {
-            if (str[i] > 96 && str[i] < 123)
-            {
-                str[i] -= 32;
-                trigger = 0;
-            }
-            else if (str[i] > 64 && str[i] < 91)
-                trigger = 0;
-            else if (str[i] > 47 && str[i] < 58)
-                trigger = 0;
-        }
-    }
-    return (str);
+	for (i = 0, trigger = 0; str[i] != '\0'; i++)
+	{
+		if (str[0] > 96 && str[0] < 123)
+			trigger = 1;
+		for (c = 0; nots[c] != '\0'; c++)
+		{
+			if (nots[c] == str[i])
+				trigger = 1;
+		}
+		if (trigger)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				trigger = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				trigger = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				trigger = 0;
+		}
+	}
+	return (str);
 }
 
