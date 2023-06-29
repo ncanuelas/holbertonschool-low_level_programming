349a6c9b532d07dff7ce666bcd90de27ce6738b6
ncanuelas <nixacanuelas@yahoo.com>
2023-06-29 14:57:20 +0000

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *function that reverses the content of an array of integers
+ void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+	tmp = a[n - 1 - index];
+	a[n - 1 - index] = a[index];
+	a[index] = tmp;
+	}
+}
