e2f4abf964079780985a2d4eaa62e09139ab0a59
ncanuelas <nixacanuelas@yahoo.com>
2023-06-29 16:25:01 +0000

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @c: string
+ * Return: return c
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+	if (c[i] > 96 && c[i] < 122)
+
+	c[i] -= 32;
+	}
+	return (c);
+}
