8a434feac557880f361ea29631da056040465818
ncanuelas <nixacanuelas@yahoo.com>
2023-06-29 15:34:51 +0000

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- *print_array - print an array of integers
- *@a: an array of integers
- *@n: the number of elements to swap
+ * function that reverses the content of an array of integers
+ * print_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
  */
 void reverse_array(int *a, int n)
 {
