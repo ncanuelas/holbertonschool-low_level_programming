f27e0db5c2171cc9f658416442ac13bebc16630d
ncanuelas <nixacanuelas@yahoo.com>
2023-07-14 17:55:36 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,42 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * string_nconcat - write a funcion that concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: unsigned int
- * Return: return 0
+ *string_nconcat- gets user inputs
+ *@s1: stores firsrt input
+ *@s2: stores second input
+ *@n: stores counter
+ *Return: results
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	int strlen1, i, c;
+	unsigned int i, x;
+	char *s3;
 
 	if (s1 == NULL)
-			s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-			s2 = "";
+		s2 = "";
 
-	strlen1 = (unsigned int)_strlen1(s1);
-	p = malloc ((strlen1 + n + 1) * sizeof(char));
-	if (p == NULL)
-			return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
+	if (n > strlen(s2))
+		n = strlen(s2);
+
+	s3 = malloc(strlen(s1) + n + 1);
+
+	if (s3 == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (i < strlen1)
-				p[i] = s1[i];
-		else
-				p[i] = s2[c++];
+		s3[i] = s1[i];
 	}
-	p[i] = '\0';
+	for (x = 0; s2[x] != '\0' && x < n; x++, i++)
+	{
+		s3[i] = s2[x];
+	}
+	s3[i] = '\0';
 
-	return (p);
-}
-
-/**
- * _strlen - find lenght of string
- * @s: string
- * Return: lenght of string
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-			;
-	return (i);
+	return (s3);
 }
