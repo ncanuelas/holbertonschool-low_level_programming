7a8a1333a673e9e6af7c377ddb7c33bda85af89b
ncanuelas <nixacanuelas@yahoo.com>
2023-08-03 00:58:26 +0000

    4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -8,11 +8,12 @@
 void free_list(list_t *head)
 {
     list_t *current = head;
+
     while (current != NULL)
     {
-	 list_t *next = current->next;
-	 free(current->str);
-	 free(current);
-	 current = next;
+        list_t *next = current->next;
+        free(current->str);
+        free(current);
+        current = next;
     }
 }
