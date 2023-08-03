8eabd34b53b7ddf8e5ca4d129fc8b62ad61284ba
ncanuelas <nixacanuelas@yahoo.com>
2023-08-03 01:08:47 +0000

    fixes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -22,3 +24,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _putchar(char c);
+#endif
